c05c3423c87a6087bd5849226fd448ac31c68b33
sangwon.yun <aiuric@naver.com>
2020-08-04 23:11:59 +0900

    slave

--- src/main/java/com/example/demo5/DemoApplication.java
+++ src/main/java/com/example/demo5/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 123
+456
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		out!!~~
